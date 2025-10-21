fd7a8a2fd11de6102876a15f08c471b6a51d0de3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 00:25:37 +0300

    Create products_remote.dart

---
+++ two_products_shop/lib/app/data/datasources/remote/products_remote.dart
@@ -0,0 +1,29 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../../services/supabase/supabase_client.dart';
+import '../../../core/config/app_constants.dart';
+import '../../models/product_model.dart';
+
+class ProductsRemote {
+  final SupabaseClient _db = Supa.client;
+
+  Future<List<ProductModel>> fetchActive() async {
+    final res = await _db
+        .from(AppConstants.tblProducts)
+        .select()
+        .eq(AppConstants.fActive, true)
+        .order(AppConstants.fId);
+    return (res as List)
+        .map((e) => ProductModel.fromMap(e as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<ProductModel?> fetchBySlug(String slug) async {
+    final res = await _db
+        .from(AppConstants.tblProducts)
+        .select()
+        .eq(AppConstants.fSlug, slug)
+        .maybeSingle();
+    if (res == null) return null;
+    return ProductModel.fromMap(res as Map<String, dynamic>);
+  }
+}
