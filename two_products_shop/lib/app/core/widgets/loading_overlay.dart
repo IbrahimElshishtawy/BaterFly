11e60b63ccaba4589421523302391e563b690226
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 09:57:42 +0300

    Create loading_overlay.dart

---
+++ two_products_shop/lib/app/core/widgets/loading_overlay.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoadingOverlay extends StatelessWidget {
+  final bool loading;
+  final Widget child;
+  const LoadingOverlay({super.key, required this.loading, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        if (loading)
+          Container(
+            color: Colors.black.withOpacity(0.4),
+            child: const Center(
+              child: CircularProgressIndicator(color: Colors.white),
+            ),
+          ),
+      ],
+    );
+  }
+}
