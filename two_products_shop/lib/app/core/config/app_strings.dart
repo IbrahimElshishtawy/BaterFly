1aa7d53dca1c015ad31f39cc9c5f9cc0814b82f1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 10:10:33 +0300

    Create app_strings.dart

---
+++ two_products_shop/lib/app/core/config/app_strings.dart
@@ -0,0 +1,9 @@
+class AppStrings {
+  static const noInternet = 'برجاء التأكد من الاتصال بالإنترنت';
+  static const noProducts = 'لا توجد منتجات حالياً';
+  static const loadError = 'حدث خطأ أثناء تحميل البيانات';
+  static const sendError = 'تعذر إرسال الطلب';
+  static const successOrder = 'تم إرسال الطلب بنجاح';
+  static const addReview = 'تم إضافة التقييم';
+  static const adminOnly = 'خاص بالأدمن';
+}
