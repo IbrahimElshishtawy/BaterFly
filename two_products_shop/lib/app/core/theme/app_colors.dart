5464bb080b3ea6b94c10cebf5e9ca6332dcbc5fa
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 09:38:27 +0300

    Create app_colors.dart

---
+++ two_products_shop/lib/app/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2196F3);
+  static const secondary = Color(0xFF00BCD4);
+  static const accent = Color(0xFFFFC107);
+  static const background = Color(0xFFF7F9FC);
+  static const textDark = Color(0xFF212121);
+  static const textLight = Color(0xFFFAFAFA);
+}
