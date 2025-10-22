2b36e992c7f40afe2b0dc38a3d08fa8d94d261e3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 10:43:12 +0300

    Create app_icons.dart

---
+++ two_products_shop/lib/app/core/config/app_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const shopping = Icons.shopping_cart;
+  static const heart = Icons.favorite;
+  static const star = Icons.star;
+  static const admin = Icons.admin_panel_settings;
+  static const settings = Icons.settings;
+}
