06056a59ed51b97a61530962fcb1bccc3acc0f08
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 14:57:54 +0300

    Update supabase_client.dart

--- two_products_shop/lib/app/services/supabase/supabase_client.dart
+++ two_products_shop/lib/app/services/supabase/supabase_client.dart
@@ -6,11 +6,12 @@ class Supa {
   static Future<void> init({String? url, String? anonKey}) async {
     const supabaseUrl = String.fromEnvironment(
       'SUPABASE_URL',
-      defaultValue: 'https://YOUR-PROJECT.supabase.co',
+      defaultValue: 'https://nqjojfeknzmyqtqpvwqb.supabase.co',
     );
     const supabaseAnon = String.fromEnvironment(
       'SUPABASE_ANON_KEY',
-      defaultValue: 'YOUR-ANON-KEY',
+      defaultValue:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xam9qZmVrbnpteXF0cXB2d3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjcyNTksImV4cCI6MjA3NjY0MzI1OX0.3DqoR9vkJqOZnNdiwEE55fAnulZpNjXQFBRbRtgznJg',
     );
 
     await Supabase.initialize(
