e3ceca366aab2eaded525f4cf9e3dc0fe005fed5
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 10:43:16 +0300

    Create image_loader.dart

---
+++ two_products_shop/lib/app/core/utils/image_loader.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ImageLoader extends StatelessWidget {
+  final String? url;
+  final double? height;
+  const ImageLoader({super.key, this.url, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    if (url == null || url!.isEmpty) {
+      return Container(
+        height: height ?? 180,
+        color: Colors.grey.shade200,
+        child: const Center(child: Icon(Icons.image_not_supported)),
+      );
+    }
+    return Image.network(
+      url!,
+      height: height,
+      width: double.infinity,
+      fit: BoxFit.cover,
+      errorBuilder: (_, __, ___) => Container(
+        height: height ?? 180,
+        color: Colors.grey.shade200,
+        child: const Center(child: Icon(Icons.broken_image)),
+      ),
+    );
+  }
+}
