85c4ac5edd24bbb4aa7b456f892571422e4344a7
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 11:06:10 +0300

    Update device_info.dart

--- two_products_shop/lib/app/core/utils/device_info.dart
+++ two_products_shop/lib/app/core/utils/device_info.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: avoid_web_libraries_in_flutter
+// ignore_for_file: avoid_web_libraries_in_flutter, dead_code
 
 import 'dart:html' as html;
 
 class DeviceInfo {
   static String get userAgent => html.window.navigator.userAgent;
-  static String get language => html.window.navigator.language ?? 'ar';
+  static String get language => html.window.navigator.language;
 }
