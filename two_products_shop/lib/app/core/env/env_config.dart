49a80ec6ff27ec9eefe57e44ce14b01231596992
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 09:57:26 +0300

    Create env_config.dart

---
+++ two_products_shop/lib/app/core/env/env_config.dart
@@ -0,0 +1,7 @@
+class EnvConfig {
+  static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+
+  static const appVersion = '1.0.0';
+  static const developer = 'ButterFly Dev Team';
+}
