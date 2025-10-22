99702636aec363d73785c9a7e79a4d2c9126e2ca
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-22 09:57:33 +0300

    Create app_routes.dart

---
+++ two_products_shop/lib/app/core/routing/app_routes.dart
@@ -0,0 +1,10 @@
+class Routes {
+  static const home = '/';
+  static const product = '/p';
+  static const checkout = '/checkout';
+  static const thankYou = '/thank-you';
+  static const admin = '/admin';
+  static const adminOrders = '/admin/orders';
+  static const adminReviews = '/admin/reviews';
+  static const adminSettings = '/admin/settings';
+}
