c6adfcb4eaf832182ce3a9fa72d9207a0499746d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 16:08:18 +0300

    Update session.dart

---
+++ two_products_shop/baterfly/lib/app/core/utils/session.dart
@@ -0,0 +1,8 @@
+class Session {
+  static String? _token;
+
+  static void setToken(String token) => _token = token;
+  static String? get token => _token;
+
+  static void clear() => _token = null;
+}
