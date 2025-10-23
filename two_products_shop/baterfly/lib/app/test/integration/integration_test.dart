d081bb729830865f5e42dffb61f8559ddf86ab49
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 17:26:27 +0300

    Update integration_test.dart

--- two_products_shop/baterfly/lib/app/test/integration/integration_test.dart
+++ two_products_shop/baterfly/lib/app/test/integration/integration_test.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:baterfly/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:  two_products_shop/main.dart';
 
 /// اختبار تكاملي بسيط لتأكيد أن التطبيق يعمل ويعرض الصفحة الأولى بدون أخطاء.
 void main() {
   testWidgets('App launches and shows home page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const ShopApp());
     await tester.pumpAndSettle();
 
     expect(find.text('Two Products Shop'), findsOneWidget);
