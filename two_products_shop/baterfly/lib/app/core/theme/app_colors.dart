a2c4859c465deef179c11465f67d31570b81ae55
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 12:20:48 +0300

    Update app_colors.dart

--- two_products_shop/baterfly/lib/app/core/theme/app_colors.dart
+++ two_products_shop/baterfly/lib/app/core/theme/app_colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Colors.pinkAccent;
-  static const secondary = Colors.orangeAccent;
-  static const background = Color(0xFFF9F9F9);
+  static const primary = Color(0xFFE91E63);
+  static const secondary = Color(0xFFFF9800);
+  static const background = Color(0xFFFDFDFD);
+  static const textDark = Color(0xFF212121);
+  static const textLight = Color(0xFF757575);
 }
