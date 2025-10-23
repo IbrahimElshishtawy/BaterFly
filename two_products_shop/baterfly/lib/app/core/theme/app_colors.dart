d2f1d7ef116551422f1a8a2fd304f454abd35f79
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 12:13:11 +0300

    Update app_colors.dart

---
+++ two_products_shop/baterfly/lib/app/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.pinkAccent;
+  static const secondary = Colors.orangeAccent;
+  static const background = Color(0xFFF9F9F9);
+}
