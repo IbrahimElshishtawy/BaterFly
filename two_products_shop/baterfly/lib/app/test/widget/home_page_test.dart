d97ea1e06538ee8a71eac3c4d643390660cdb304
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 17:26:35 +0300

    Update home_page_test.dart

---
+++ two_products_shop/baterfly/lib/app/test/widget/home_page_test.dart
@@ -0,0 +1,15 @@
+import 'package:baterfly/app/features/catalog/pages/home_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  testWidgets('HomePage loads and shows loading indicator', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+
+    // يجب أن يظهر مؤشر التحميل أولاً
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    // انتظار اكتمال البيانات الوهمية
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+  });
+}
