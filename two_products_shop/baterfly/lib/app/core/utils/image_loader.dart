8041232708d4b1781fe3cabdf28dc621111fad08
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 16:08:12 +0300

    Update image_loader.dart

---
+++ two_products_shop/baterfly/lib/app/core/utils/image_loader.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageLoader extends StatelessWidget {
+  final String url;
+  final double? height;
+  final double? width;
+  final BoxFit fit;
+  const ImageLoader({
+    super.key,
+    required this.url,
+    this.height,
+    this.width,
+    this.fit = BoxFit.cover,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (url.isEmpty) {
+      return const Icon(Icons.image_not_supported, color: Colors.grey);
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Image.network(
+        url,
+        height: height,
+        width: width,
+        fit: fit,
+        errorBuilder: (_, __, ___) => const Icon(Icons.broken_image),
+      ),
+    );
+  }
+}
