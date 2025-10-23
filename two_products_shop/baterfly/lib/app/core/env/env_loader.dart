4fe7a348af2e4bc0d6b4bb6c7e48caf02e9b9cee
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 20:34:14 +0300

    Update env_loader.dart

--- two_products_shop/baterfly/lib/app/core/env/env_loader.dart
+++ two_products_shop/baterfly/lib/app/core/env/env_loader.dart
@@ -1,12 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static Future<void> load() async => await dotenv.load();
-
-  static String get url =>
-      dotenv.env['https://nqjojfeknzmyqtqpvwqb.supabase.co'] ?? '';
-  static String get anonKey =>
-      dotenv
-          .env['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xam9qZmVrbnpteXF0cXB2d3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjcyNTksImV4cCI6MjA3NjY0MzI1OX0.3DqoR9vkJqOZnNdiwEE55fAnulZpNjXQFBRbRtgznJg'] ??
-      '';
+  static Future<void> load() async =>
+      dotenv.load(fileName: 'lib/app/assets/.env');
+  static String get url => dotenv.env['SUPABASE_URL'] ?? '';
+  static String get anonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 }
