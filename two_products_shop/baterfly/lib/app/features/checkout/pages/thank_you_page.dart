191cb6e4c478a4173c6da01995f64ff3c451852e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 12:24:07 +0300

    Update thank_you_page.dart

--- two_products_shop/baterfly/lib/app/features/checkout/pages/thank_you_page.dart
+++ two_products_shop/baterfly/lib/app/features/checkout/pages/thank_you_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../core/theme/app_text_styles.dart';
 
 class ThankYouPage extends StatelessWidget {
   const ThankYouPage({super.key});
@@ -7,31 +8,18 @@ class ThankYouPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TweenAnimationBuilder<double>(
-          duration: const Duration(seconds: 2),
-          tween: Tween(begin: 0, end: 1),
-          builder: (context, value, child) {
-            return Opacity(
-              opacity: value,
-              child: Transform.scale(scale: value, child: child),
-            );
-          },
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Icon(Icons.check_circle, color: Colors.green, size: 100),
-              SizedBox(height: 20),
-              Text(
-                'تم تأكيد الطلب بنجاح!',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'سيتم التواصل معك قريبًا لتأكيد التفاصيل.',
-                style: TextStyle(fontSize: 16, color: Colors.black54),
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Icon(Icons.check_circle, color: Colors.green, size: 100),
+            SizedBox(height: 20),
+            Text('تم تأكيد الطلب بنجاح!', style: AppTextStyles.title),
+            SizedBox(height: 10),
+            Text(
+              'سيتم التواصل معك قريبًا لتأكيد التفاصيل.',
+              style: AppTextStyles.subtitle,
+            ),
+          ],
         ),
       ),
     );
