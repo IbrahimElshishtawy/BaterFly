6c9baffbc3dabe7ce7d353c0ca5c96cefe3ac484
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-23 12:20:38 +0300

    Update app_router.dart

--- two_products_shop/baterfly/lib/app/core/routing/app_router.dart
+++ two_products_shop/baterfly/lib/app/core/routing/app_router.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import '../../features/catalog/pages/home_page.dart';
+import '../../features/product/pages/product_page.dart';
+import '../../features/checkout/pages/checkout_page.dart';
+import 'app_routes.dart';
 
 class AppRouter {
-  static Route<dynamic> onGenerate(RouteSettings s) {
-    switch (s.name) {
-      case '/':
+  static Route<dynamic> onGenerate(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.home:
         return MaterialPageRoute(builder: (_) => const HomePage());
+      case AppRoutes.product:
+        final p = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(builder: (_) => ProductPage(product: p));
+      case AppRoutes.checkout:
+        final p = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(builder: (_) => CheckoutPage(product: p));
       default:
         return MaterialPageRoute(
           builder: (_) =>
-              const Scaffold(body: Center(child: Text('Page not found'))),
+              const Scaffold(body: Center(child: Text('Page Not Found'))),
         );
     }
   }
