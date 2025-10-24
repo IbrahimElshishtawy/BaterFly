272302c10717d958f342e17844e736924489b043
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-24 18:19:06 +0300

    Update supabase_service.dart

--- two_products_shop/baterfly/lib/app/services/supabase/supabase_service.dart
+++ two_products_shop/baterfly/lib/app/services/supabase/supabase_service.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../../core/env/env_loader.dart';
 
 class Supa {
-  static SupabaseClient get client => SupabaseClient(Env.url, Env.anonKey);
+  static SupabaseClient get client => Supabase.instance.client;
 
   static Future<void> init() async {
-    await Supabase.initialize(url: Env.url, anonKey: Env.anonKey);
+    await Supabase.initialize(
+      url: 'https://nqjojfeknzmyqtqpvwqb.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xam9qZmVrbnpteXF0cXB2d3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjcyNTksImV4cCI6MjA3NjY0MzI1OX0.3DqoR9vkJqOZnNdiwEE55fAnulZpNjXQFBRbRtgznJg',
+      authOptions: FlutterAuthClientOptions(autoRefreshToken: true),
+    );
+
+    if (kDebugMode) {
+      print('✅ Supabase initialized successfully');
+    }
   }
 }
