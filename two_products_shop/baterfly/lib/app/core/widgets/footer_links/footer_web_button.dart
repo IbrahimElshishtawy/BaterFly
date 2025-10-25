b900a34ce80cf2fd584866fc24aacd9ab8bc6db5
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-25 21:52:29 +0300

    Create footer_web_button.dart

---
+++ two_products_shop/baterfly/lib/app/core/widgets/footer_links/footer_web_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class FooterWebButton extends StatefulWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onTap;
+  const FooterWebButton({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  State<FooterWebButton> createState() => _FooterWebButtonState();
+}
+
+class _FooterWebButtonState extends State<FooterWebButton> {
+  bool _hover = false;
+  bool _down = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final bg = _down
+        ? const Color(0xFF1F2A3A)
+        : (_hover ? const Color(0xFF1B2433) : const Color(0x191C2736));
+    final border = _hover ? const Color(0x3340A9FF) : const Color(0x221C86FF);
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hover = true),
+      onExit: (_) => setState(() {
+        _hover = false;
+        _down = false;
+      }),
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTapDown: (_) => setState(() => _down = true),
+        onTapCancel: () => setState(() => _down = false),
+        onTapUp: (_) => setState(() => _down = false),
+        onTap: widget.onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 140),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+          decoration: BoxDecoration(
+            color: bg,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: border),
+            boxShadow: _hover
+                ? const [
+                    BoxShadow(
+                      color: Color(0x22000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 6),
+                    ),
+                  ]
+                : const [],
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(widget.icon, size: 18, color: Colors.white),
+              const SizedBox(width: 8),
+              Text(
+                widget.label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
