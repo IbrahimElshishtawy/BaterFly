7672d2f73be32ffb026e7ed798801b365264afd1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-25 20:58:59 +0300

    Update app_routes.dart

--- two_products_shop/baterfly/lib/app/core/routing/app_routes.dart
+++ two_products_shop/baterfly/lib/app/core/routing/app_routes.dart
@@ -1,5 +1,6 @@
 class AppRoutes {
   static const home = '/';
+  static const catalog = '/catalog'; // جديد
   static const product = '/product';
   static const checkout = '/checkout';
   static const adminLogin = '/admin/login';
