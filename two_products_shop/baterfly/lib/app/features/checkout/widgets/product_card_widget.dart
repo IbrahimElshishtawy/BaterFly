64d45622ef8d1906d2cda43398630669305d55d3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-27 20:52:47 +0300

    Create product_card_widget.dart

---
+++ two_products_shop/baterfly/lib/app/features/checkout/widgets/product_card_widget.dart
@@ -0,0 +1,65 @@
+// lib/app/features/catalog/widgets/product_card_widget.dart
+import 'package:flutter/material.dart';
+
+class ProductCardWidget extends StatelessWidget {
+  final Map<String, dynamic> product;
+
+  const ProductCardWidget({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    final p = product;
+    final price = (p['price'] as num).toDouble();
+    final total = price * 1;
+
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+        side: const BorderSide(color: Colors.black12),
+      ),
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                (p['image'] as String?) ?? 'assets/images/image_1.jpg',
+
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    p['name'],
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    '$total ج.م',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 16,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
