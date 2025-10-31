930769ac091afaaf98a22425ab49de6800a1611d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-31 15:48:38 +0200

    Update usp_bar.dart

--- two_products_shop/baterfly/lib/app/features/product/widgets/usp_bar.dart
+++ two_products_shop/baterfly/lib/app/features/product/widgets/usp_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class USPBar extends StatelessWidget {
-  const USPBar();
+  const USPBar({super.key});
 
   @override
   Widget build(BuildContext context) {
