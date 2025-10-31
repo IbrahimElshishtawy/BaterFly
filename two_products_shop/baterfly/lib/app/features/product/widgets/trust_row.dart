266a10e5fcccd3001e91e9b473d2294899e8c165
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-31 15:48:31 +0200

    Update trust_row.dart

--- two_products_shop/baterfly/lib/app/features/product/widgets/trust_row.dart
+++ two_products_shop/baterfly/lib/app/features/product/widgets/trust_row.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TrustRow extends StatelessWidget {
-  const TrustRow();
+  const TrustRow({super.key});
 
   @override
   Widget build(BuildContext context) {
