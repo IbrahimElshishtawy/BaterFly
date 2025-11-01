62525cf4db07e27b8beec6d129a1b284819ac3dd
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-01 20:55:22 +0200

    Update product_video_widget.dart

--- two_products_shop/baterfly/lib/app/core/widgets/product_video_widget.dart
+++ two_products_shop/baterfly/lib/app/core/widgets/product_video_widget.dart
@@ -1,21 +1,16 @@
-// المسار المقترح: lib/app/core/widgets/product_video_widget.dart
-
-// ignore_for_file: use_super_parameters, library_private_types_in_public_api, deprecated_member_use
+// ignore_for_file: use_super_parameters, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class ProductVideoWidget extends StatefulWidget {
   final String videoUrl;
-  final double aspectRatio;
-  const ProductVideoWidget({
-    Key? key,
-    required this.videoUrl,
-    this.aspectRatio = 16 / 9,
-  }) : super(key: key);
+  // مش محتاجين نحدد نسبة ثابتة هنا كمان
+  const ProductVideoWidget({Key? key, required this.videoUrl})
+    : super(key: key);
 
   @override
-  _ProductVideoWidgetState createState() => _ProductVideoWidgetState();
+  State<ProductVideoWidget> createState() => _ProductVideoWidgetState();
 }
 
 class _ProductVideoWidgetState extends State<ProductVideoWidget> {
@@ -23,12 +18,22 @@ class _ProductVideoWidgetState extends State<ProductVideoWidget> {
   bool _isPlaying = false;
   bool _isInitialized = false;
 
+  // 1. متغير جديد عشان نخزن فيه نسبة العرض للارتفاع الحقيقية للفيديو
+  double? _videoAspectRatio;
+
   @override
   void initState() {
     super.initState();
 
-    _controller = VideoPlayerController.network(widget.videoUrl)
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
       ..initialize().then((_) {
+        // 2. لما الفيديو يخلص التحميل، بنحسب النسبة من أبعاده الحقيقية
+        if (_controller.value.size.width > 0 &&
+            _controller.value.size.height > 0) {
+          _videoAspectRatio =
+              _controller.value.size.width / _controller.value.size.height;
+        }
+
         setState(() {
           _isInitialized = true;
         });
@@ -64,56 +69,56 @@ class _ProductVideoWidgetState extends State<ProductVideoWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return _isInitialized
-        ? Column(
-            children: [
-              AspectRatio(
-                aspectRatio: widget.aspectRatio,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    VideoPlayer(_controller),
-                    // زر التشغيل/الإيقاف المخصص
-                    if (!_isPlaying)
-                      GestureDetector(
-                        onTap: _togglePlayPause,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.5),
-                            shape: BoxShape.circle,
-                          ),
-                          child: const Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                            size: 60,
-                          ),
-                        ),
-                      ),
-                    // شريط التقدم
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: VideoProgressIndicator(
-                        _controller,
-                        allowScrubbing: true,
-                        colors: const VideoProgressColors(
-                          playedColor: Colors.blue,
-                          backgroundColor: Colors.grey,
-                          bufferedColor: Colors.lightBlue,
-                        ),
-                      ),
-                    ),
-                  ],
+    // لو الفيديو مزلش اتحمل أو نسبة العرض للارتفاع مش معروفة
+    if (!_isInitialized || _videoAspectRatio == null) {
+      return Container(
+        width: double.infinity, // اخد العرض الكامل
+        height: 200,
+        color: Colors.black12,
+        child: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    // 3. بنستخدم النسبة اللي حسبناها من الفيديو نفسه
+    return AspectRatio(
+      aspectRatio: _videoAspectRatio!,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          VideoPlayer(_controller),
+          // زر التشغيل/الإيقاف المخصص
+          if (!_isPlaying)
+            GestureDetector(
+              onTap: _togglePlayPause,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 60,
                 ),
               ),
-              const SizedBox(height: 16),
-            ],
-          )
-        : Container(
-            height: 200,
-            color: Colors.black12,
-            child: const Center(child: CircularProgressIndicator()),
-          );
+            ),
+          // شريط التقدم
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: VideoProgressIndicator(
+              _controller,
+              allowScrubbing: true,
+              colors: const VideoProgressColors(
+                playedColor: Colors.blue,
+                backgroundColor: Colors.grey,
+                bufferedColor: Colors.lightBlue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
