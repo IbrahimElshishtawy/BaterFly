5858a585426df4d48da7c3c705c88fb31b8a3736
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-04 19:32:00 +0200

    Update footer_helpers.dart

--- two_products_shop/baterfly/lib/app/core/widgets/footer_links/footer_helpers.dart
+++ two_products_shop/baterfly/lib/app/core/widgets/footer_links/footer_helpers.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -45,6 +44,7 @@ class FooterLink extends StatefulWidget {
 
 class _FooterLinkState extends State<FooterLink> {
   bool _hover = false;
+
   @override
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme.bodySmall?.copyWith(
@@ -73,6 +73,45 @@ class _FooterLinkState extends State<FooterLink> {
             padding: const EdgeInsets.symmetric(vertical: 2),
             child: Text(widget.text, style: style),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class FooterIconLink extends StatefulWidget {
+  final String url;
+  final IconData icon;
+  const FooterIconLink({super.key, required this.url, required this.icon});
+
+  @override
+  State<FooterIconLink> createState() => _FooterIconLinkState();
+}
+
+class _FooterIconLinkState extends State<FooterIconLink> {
+  bool _hover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.white.withOpacity(_hover ? .95 : .85);
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hover = true),
+      onExit: (_) => setState(() => _hover = false),
+      cursor: SystemMouseCursors.click,
+      child: InkResponse(
+        onTap: () => launchUrl(
+          Uri.parse(widget.url),
+          mode: LaunchMode.externalApplication,
+        ),
+        radius: 22,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 140),
+          padding: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: _hover ? Colors.white.withOpacity(0.08) : Colors.transparent,
+          ),
+          child: Icon(widget.icon, size: 20, color: color),
         ),
       ),
     );
