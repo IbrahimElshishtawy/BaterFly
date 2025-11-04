10b6a40ebb072bf4cd7cb9f1d312caa1676b73f1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-04 13:27:47 +0200

    Update product_title.dart

--- two_products_shop/baterfly/lib/app/features/product/sections/product_title.dart
+++ two_products_shop/baterfly/lib/app/features/product/sections/product_title.dart
@@ -1,20 +1,18 @@
-// lib/app/features/product/widgets/product_title.dart
 import 'package:flutter/material.dart';
 
 class ProductTitle extends StatelessWidget {
-  final String name;
-
-  const ProductTitle({super.key, required this.name});
+  final Map product;
+  const ProductTitle({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 18, bottom: 8),
+      padding: const EdgeInsets.all(20),
       child: Text(
-        name,
-        style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-          fontWeight: FontWeight.w900,
-          letterSpacing: .2,
+        product['name'] ?? 'Unnamed Product',
+        style: const TextStyle(
+          fontSize: 26,
+          fontWeight: FontWeight.bold,
           color: Colors.white,
         ),
       ),
