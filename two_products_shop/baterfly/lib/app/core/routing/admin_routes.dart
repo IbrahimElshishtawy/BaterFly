f8d2e94538a51ab8e5e38c2ce78e6f3ea2d60ab5
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-06 14:39:23 +0200

    Update admin_routes.dart

---
+++ two_products_shop/baterfly/lib/app/core/routing/admin_routes.dart
@@ -0,0 +1,8 @@
+// admin_routes.dart
+class AdminRoutes {
+  static const dashboard = '/admin';
+  static const orders = '/admin/orders';
+  static const products = '/admin/products';
+  static const reviews = '/admin/reviews';
+  static const settings = '/admin/settings';
+}
