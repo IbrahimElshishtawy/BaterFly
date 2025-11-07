4b12012e558bc1b8c7aa48cccd940b549d2c608f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-07 16:14:30 +0200

    Update admin_dashboard_page.dart

--- two_products_shop/baterfly/lib/app/features/admin/pages/admin_dashboard_page.dart
+++ two_products_shop/baterfly/lib/app/features/admin/pages/admin_dashboard_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:baterfly/app/features/admin/pages/products_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
