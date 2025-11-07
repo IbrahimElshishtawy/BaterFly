5621c36ae7ab1a061cda779abd70d353a66d76c6
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-07 16:14:25 +0200

    Update review_model.dart

--- two_products_shop/baterfly/lib/app/data/models/review_model.dart
+++ two_products_shop/baterfly/lib/app/data/models/review_model.dart
@@ -1,23 +1,50 @@
+// ignore_for_file: overridden_fields
+
 import '../../domain/entities/review.dart';
 
 class ReviewModel extends Review {
+  @override
+  final int id;
+  @override
+  final int productId;
+  @override
+  final String fullName;
+  @override
+  final int rating;
+  @override
+  final String comment;
+  @override
+  final bool isVerified;
+  @override
+  final String status;
+
   ReviewModel({
-    required super.id,
-    required super.productId,
-    required super.fullName,
-    required super.rating,
-    required super.comment,
-    required super.isVerified,
-    required super.status,
-  });
+    required this.id,
+    required this.productId,
+    required this.fullName,
+    required this.rating,
+    required this.comment,
+    required this.isVerified,
+    required this.status,
+  }) : super(
+         id: id,
+         productId: productId,
+         fullName: fullName,
+         rating: rating,
+         comment: comment,
+         isVerified: isVerified,
+         status: status,
+       );
 
   factory ReviewModel.fromMap(Map<String, dynamic> map) {
     return ReviewModel(
-      id: map['id'],
-      productId: map['product_id'],
-      fullName: map['full_name'],
-      rating: map['rating'],
-      comment: map['comment'],
+      id: map['id']?.toint() ?? '',
+      productId: map['product_id'] ?? 0,
+      fullName: map['full_name'] ?? '',
+      rating: (map['rating'] is int)
+          ? map['rating']
+          : int.tryParse(map['rating'].toString()) ?? 0,
+      comment: map['comment'] ?? '',
       isVerified: map['is_verified'] ?? false,
       status: map['status'] ?? 'pending',
     );
