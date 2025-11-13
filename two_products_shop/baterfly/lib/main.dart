379630dc4cb1c59c1a698c4547ba0231f5af450a
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-13 18:37:07 +0200

    Update main.dart

--- two_products_shop/baterfly/lib/main.dart
+++ two_products_shop/baterfly/lib/main.dart
@@ -19,7 +19,7 @@ class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BatteryFly',
+      title: 'ButteryFly',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       initialRoute: AppRoutes.home,
