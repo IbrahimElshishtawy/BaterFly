1b7e2f366b8c4c2ca2d96a6c7682d26edf1afb5d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 01:21:20 +0200

    Update Product_Images.dart

--- two_products_shop/baterfly/lib/app/features/product/sections/Product_Images.dart
+++ two_products_shop/baterfly/lib/app/features/product/sections/Product_Images.dart
@@ -1,29 +1,34 @@
 // ignore_for_file: file_names
 
-import 'package:baterfly/app/data/static/product_data.dart';
 import 'package:flutter/material.dart';
 
 class ProductImages extends StatelessWidget {
-  const ProductImages({super.key});
+  final List<String> images;
+
+  const ProductImages({super.key, required this.images});
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    final width = MediaQuery.of(context).size.width;
 
     return SizedBox(
       height: 250,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: ProductData.images.length,
+        itemCount: images.length,
         padding: const EdgeInsets.symmetric(horizontal: 12),
         itemBuilder: (context, index) {
+          final imagePath = images[index];
+
           return GestureDetector(
             onTap: () {
-              // فتح Fullscreen عند الضغط على الصورة
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => FullscreenImageViewer(initialIndex: index),
+                  builder: (_) => FullscreenImageViewer(
+                    images: images,
+                    initialIndex: index,
+                  ),
                 ),
               );
             },
@@ -32,7 +37,7 @@ class ProductImages extends StatelessWidget {
               margin: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
                     color: Colors.black26,
                     blurRadius: 8,
@@ -40,7 +45,7 @@ class ProductImages extends StatelessWidget {
                   ),
                 ],
                 image: DecorationImage(
-                  image: AssetImage(ProductData.images[index]),
+                  image: AssetImage(imagePath),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -53,8 +58,14 @@ class ProductImages extends StatelessWidget {
 }
 
 class FullscreenImageViewer extends StatefulWidget {
+  final List<String> images;
   final int initialIndex;
-  const FullscreenImageViewer({super.key, required this.initialIndex});
+
+  const FullscreenImageViewer({
+    super.key,
+    required this.images,
+    required this.initialIndex,
+  });
 
   @override
   State<FullscreenImageViewer> createState() => _FullscreenImageViewerState();
@@ -63,6 +74,8 @@ class FullscreenImageViewer extends StatefulWidget {
 class _FullscreenImageViewerState extends State<FullscreenImageViewer> {
   late PageController _pageController;
   late int currentIndex;
+
+  List<String> get images => widget.images;
 
   @override
   void initState() {
@@ -79,7 +92,7 @@ class _FullscreenImageViewerState extends State<FullscreenImageViewer> {
         backgroundColor: const Color.fromARGB(0, 246, 245, 245),
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.close),
+          icon: const Icon(Icons.close),
           onPressed: () => Navigator.pop(context),
         ),
       ),
@@ -88,32 +101,29 @@ class _FullscreenImageViewerState extends State<FullscreenImageViewer> {
           Expanded(
             child: PageView.builder(
               controller: _pageController,
-              itemCount: ProductData.images.length,
+              itemCount: images.length,
               onPageChanged: (index) {
                 setState(() {
                   currentIndex = index;
                 });
               },
               itemBuilder: (context, index) {
+                final imagePath = images[index];
                 return InteractiveViewer(
                   panEnabled: true,
                   minScale: 1,
                   maxScale: 4,
-                  child: Image.asset(
-                    ProductData.images[index],
-                    fit: BoxFit.contain,
-                  ),
+                  child: Image.asset(imagePath, fit: BoxFit.contain),
                 );
               },
             ),
           ),
-          // Dots Indicator
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 12),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(ProductData.images.length, (index) {
-                bool isActive = currentIndex == index;
+              children: List.generate(images.length, (index) {
+                final isActive = currentIndex == index;
                 return AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
                   margin: const EdgeInsets.symmetric(horizontal: 4),
