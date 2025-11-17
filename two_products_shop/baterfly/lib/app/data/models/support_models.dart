e9c81204d66ff2138191be9b3a00e0d3f9bb8d6d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-17 21:08:54 +0200

    Update support_models.dart

--- two_products_shop/baterfly/lib/app/data/models/support_models.dart
+++ two_products_shop/baterfly/lib/app/data/models/support_models.dart
@@ -1,11 +1,14 @@
-// models/support_models.dart
+// app/data/models/support_models.dart
+
 class SupportContactModel {
+  final int? id;
   final String title;
   final String body;
   final String url;
   final String type;
 
   SupportContactModel({
+    this.id,
     required this.title,
     required this.body,
     required this.url,
@@ -14,10 +17,27 @@ class SupportContactModel {
 
   factory SupportContactModel.fromJson(Map<String, dynamic> json) {
     return SupportContactModel(
+      id: json['id'] as int?,
       title: json['title'] as String,
       body: json['body'] as String,
-      url: json['url'] as String,
+      url: json['url'] as String? ?? '',
       type: json['type'] as String,
+    );
+  }
+
+  SupportContactModel copyWith({
+    int? id,
+    String? title,
+    String? body,
+    String? url,
+    String? type,
+  }) {
+    return SupportContactModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+      url: url ?? this.url,
+      type: type ?? this.type,
     );
   }
 }
